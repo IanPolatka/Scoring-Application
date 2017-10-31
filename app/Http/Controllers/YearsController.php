86a6cc7a536e49ab718308bf98f5fccdd7ef33b8
Ian Polatka <ianpolatka@gmail.com>
2017-10-31 08:57:13 -0400

    Show All Years In API Route

--- app/Http/Controllers/YearsController.php
+++ app/Http/Controllers/YearsController.php
@@ -11,7 +11,7 @@ class YearsController extends Controller
 
 	public function __construct() 
 	{
-	  $this->middleware('auth');
+	  $this->middleware('auth', ['except' => ['showall']]);
 	}
     
 	public function index()
