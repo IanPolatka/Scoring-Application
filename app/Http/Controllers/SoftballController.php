63fd76563733e3681f2810b67c5ce9dd87213e44
Ian Polatka <ianpolatka@gmail.com>
2018-03-04 22:33:52 -0500

    Added controller for softball api team schedule route.

--- app/Http/Controllers/SoftballController.php
+++ app/Http/Controllers/SoftballController.php
@@ -177,6 +177,19 @@ class SoftballController extends Controller
 		$softball = Softball::join('years', 'softball.year_id', 'years.id')
 							->select('softball.*')
 							->where('year_id', '=', $selectedyearid)
+							->where('team_level', '=', 1)
+							->where(function ($query) use ($selectedteamid) {
+						        $query->where('away_team_id', '=' , $selectedteamid)
+						            ->orWhere('home_team_id', '=', $selectedteamid);
+						    })
+						    ->orderBy('date')
+							->get();
+
+		//  Display schedule for team based on selected year
+		$jvsoftball = Softball::join('years', 'softball.year_id', 'years.id')
+							->select('softball.*')
+							->where('year_id', '=', $selectedyearid)
+							->where('team_level', '=', 2)
 							->where(function ($query) use ($selectedteamid) {
 						        $query->where('away_team_id', '=' , $selectedteamid)
 						            ->orWhere('home_team_id', '=', $selectedteamid);
@@ -187,7 +200,8 @@ class SoftballController extends Controller
 		$region 		= Team::where('school_name', $team)->pluck('region_baseball');
 		$standings		= Team::where('region_baseball', $region)->orderBy('school_name')->get();
 
-		return view('sports.softball.teamschedule', compact('softball', 
+		return view('sports.softball.teamschedule', compact('softball',
+															'jvsoftball', 
 															'selectedteam',
 															'selectedteamid',
 															'selectedyear',
@@ -229,5 +243,79 @@ class SoftballController extends Controller
 		return view('sports.softball.yearschedule', compact('softball', 'selectedyear', 'selectedyearid', 'teams', 'year', 'years'));
 
 	}
+
+
+
+
+
+
+
+
+	public function apiteamschedule($year, $team, $teamlevel)
+	{
+
+		$theteam = Team::where('school_name', '=', $team)->pluck('id');
+
+		$softball = Softball::join('teams as home_team', 'softball.home_team_id', '=', 'home_team.id')
+							->join('teams as away_team', 'softball.away_team_id', '=', 'away_team.id')
+							->join('years', 'softball.year_id', '=', 'years.id')
+							->join('times', 'softball.time_id', '=', 'times.id')
+							->leftjoin('teams as winner', 'softball.winning_team', '=', 'winner.id')
+							->leftjoin('teams as loser', 'softball.losing_team', '=', 'loser.id')
+							->select(
+									'softball.id',
+									'softball.date',
+									'year',
+									'scrimmage',
+									'time',
+									'softball.tournament_title',
+									'away_team.school_name as away_team',
+									'away_team.logo as away_team_logo',
+									'away_team.mascot as away_team_mascot',
+									'away_team.district_softball as away_team_softball_district',
+									'away_team.region_softball as away_team_softball_region',
+									'softball.away_team_first_inning_score',
+									'softball.away_team_second_inning_score',
+									'softball.away_team_third_inning_score',
+									'softball.away_team_fourth_inning_score',
+									'softball.away_team_fifth_inning_score',
+									'softball.away_team_sixth_inning_score',
+									'softball.away_team_seventh_inning_score',
+									'softball.away_team_extra_inning_score',
+									'softball.away_team_final_score',
+									'home_team.school_name as home_team',
+									'home_team.logo as home_team_logo',
+									'home_team.mascot as home_team_mascot',
+									'home_team.district_softball as home_team_softball_district',
+									'home_team.region_softball as home_team_softball_region',
+									'softball.home_team_first_inning_score',
+									'softball.home_team_second_inning_score',
+									'softball.home_team_third_inning_score',
+									'softball.home_team_fourth_inning_score',
+									'softball.home_team_fifth_inning_score',
+									'softball.home_team_sixth_inning_score',
+									'softball.home_team_seventh_inning_score',
+									'softball.home_team_extra_inning_score',
+									'softball.home_team_final_score',
+									'softball.game_status',
+									'softball.winning_team',
+									'softball.losing_team',
+									'softball.team_level',
+									'winner.school_name as winning_team',
+									'loser.school_name as losing_team'
+								)
+							->where('year', '=', $year)
+							->where(function ($query) use ($theteam) {
+						        $query->where('away_team_id', '=' , $theteam)
+						            ->orWhere('home_team_id', '=', $theteam);
+						    })
+							->where('team_level', '=', $teamlevel)
+							->orderBy('date')
+					    	->get();
+
+		return $softball;
+
+	}
+
 
 }
