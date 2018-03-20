e6727974dc7d70f16dc719066ef4c3209986fc0d
Ian Polatka <ianpolatka@gmail.com>
2018-03-19 22:40:35 -0400

    Girls Tennis year schedule api route added.

--- app/Http/Controllers/SoftballController.php
+++ app/Http/Controllers/SoftballController.php
@@ -197,8 +197,8 @@ class SoftballController extends Controller
 						    ->orderBy('date')
 							->get();
 
-		$region 		= Team::where('school_name', $team)->pluck('region_baseball');
-		$standings		= Team::where('region_baseball', $region)->orderBy('school_name')->get();
+		$region 		= Team::where('school_name', $team)->pluck('region_softball');
+		$standings		= Team::where('region_softball', $region)->orderBy('school_name')->get();
 
 		return view('sports.softball.teamschedule', compact('softball',
 															'jvsoftball', 
@@ -311,6 +311,70 @@ class SoftballController extends Controller
 						    })
 							->where('team_level', '=', $teamlevel)
 							->orderBy('date')
+					    	->get();
+
+		return $softball;
+
+	}
+
+
+
+	public function todaysevents($team)
+	{
+
+		$today = Carbon::today();
+
+		// return $today;
+
+		$theteam = Team::where('school_name', '=', $team)->pluck('id');
+
+		$softball = Softball::join('teams as home_team', 'softball.home_team_id', '=', 'home_team.id')
+							->join('teams as away_team', 'softball.away_team_id', '=', 'away_team.id')
+							->join('years', 'softball.year_id', '=', 'years.id')
+							->join('times', 'softball.time_id', '=', 'times.id')
+							->select(
+									'softball.id',
+									'softball.date',
+									'year',
+									'scrimmage',
+									'time',
+									'softball.tournament_title',
+									'away_team.school_name as away_team',
+									'away_team.logo as away_team_logo',
+									'away_team.mascot as away_team_mascot',
+									'softball.away_team_first_inning_score',
+									'softball.away_team_second_inning_score',
+									'softball.away_team_third_inning_score',
+									'softball.away_team_fourth_inning_score',
+									'softball.away_team_fifth_inning_score',
+									'softball.away_team_sixth_inning_score',
+									'softball.away_team_seventh_inning_score',
+									'softball.away_team_extra_inning_score',
+									'softball.away_team_final_score',
+									'home_team.school_name as home_team',
+									'home_team.logo as home_team_logo',
+									'home_team.mascot as home_team_mascot',
+									'softball.home_team_first_inning_score',
+									'softball.home_team_second_inning_score',
+									'softball.home_team_third_inning_score',
+									'softball.home_team_fourth_inning_score',
+									'softball.home_team_fifth_inning_score',
+									'softball.home_team_sixth_inning_score',
+									'softball.home_team_seventh_inning_score',
+									'softball.home_team_extra_inning_score',
+									'softball.home_team_final_score',
+									'softball.game_status',
+									'softball.winning_team',
+									'softball.losing_team',
+									'softball.team_level'
+								)
+							->where('softball.team_level', '=', 1)
+							->where(function ($query) use ($theteam) {
+						        $query->where('away_team_id', '=' , $theteam)
+						            ->orWhere('home_team_id', '=', $theteam);
+						    })
+							->where('date', '=', $today)
+    						->orderBy('time')
 					    	->get();
 
 		return $softball;
