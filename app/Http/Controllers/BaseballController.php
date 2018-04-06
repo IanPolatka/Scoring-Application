b69f4e96ce459f58cc43cd20c407243c211f4b9b
Ian Polatka <ianpolatka@gmail.com>
2018-04-06 08:47:36 -0400

    Baseball game api route added.

--- app/Http/Controllers/BaseballController.php
+++ app/Http/Controllers/BaseballController.php
@@ -365,6 +365,65 @@ class BaseballController extends Controller
 		return $baseball;
 
 	}
+	
+	
+	
+	public function apigame($id)
+	{
+
+		$baseball = Baseball::join('teams as home_team', 'baseball.home_team_id', '=', 'home_team.id')
+							->join('teams as away_team', 'baseball.away_team_id', '=', 'away_team.id')
+							->join('years', 'baseball.year_id', '=', 'years.id')
+							->join('times', 'baseball.time_id', '=', 'times.id')
+							->select(
+									'baseball.id',
+									'baseball.date',
+									'year',
+									'scrimmage',
+									'time',
+									'baseball.tournament_title',
+									'away_team_id',
+									'away_team.school_name as away_team',
+									'away_team.abbreviated_name as away_team_abbreviated_name',
+									'away_team.mascot as away_team_mascot',
+									'away_team.logo as away_team_logo',
+									'away_team.city as away_team_city',
+									'away_team.state as away_team_state',
+									'baseball.away_team_first_inning_score',
+									'baseball.away_team_second_inning_score',
+									'baseball.away_team_third_inning_score',
+									'baseball.away_team_fourth_inning_score',
+									'baseball.away_team_fifth_inning_score',
+									'baseball.away_team_sixth_inning_score',
+									'baseball.away_team_seventh_inning_score',
+									'baseball.away_team_extra_inning_score',
+									'baseball.away_team_final_score',
+									'home_team_id',
+									'home_team.school_name as home_team',
+									'home_team.abbreviated_name as home_team_abbreviated_name',
+									'home_team.mascot as home_team_mascot',
+									'home_team.logo as home_team_logo',
+									'home_team.city as home_team_city',
+									'home_team.state as home_team_state',
+									'baseball.home_team_first_inning_score',
+									'baseball.home_team_second_inning_score',
+									'baseball.home_team_third_inning_score',
+									'baseball.home_team_fourth_inning_score',
+									'baseball.home_team_fifth_inning_score',
+									'baseball.home_team_sixth_inning_score',
+									'baseball.home_team_seventh_inning_score',
+									'baseball.home_team_extra_inning_score',
+									'baseball.home_team_final_score',
+									'baseball.game_status',
+									'baseball.winning_team',
+									'baseball.losing_team'
+								)
+							->where('baseball.id', '=', $id)
+					    	->get();
+
+		return $baseball;
+
+	}
 
 
 
